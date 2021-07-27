4a065e7c85e0d8e3ca3764808268cf71e3f19e76
Miggy Escalona <miggyescalona@users.noreply.github.com>
2021-07-27 16:19:26 -0400

    Add files via upload

---
+++ BLEND_MR_CloseHybridAttachCSV.js
@@ -0,0 +1,141 @@
+/**
+ *@NApiVersion 2.1
+ *@NScriptType MapReduceScript
+ */
+
+ 
+  /**
+ * 
+ * Date : 16 June 2021  
+ * Author : Paolo Escalona
+ * 
+ *  Date Modified       Modified By         Notes
+ *  16 June 2021          Paolo Escalona      Initial Version
+ */
+
+
+var MR_OBJ = {
+    SEARCH: {
+        HYBRID: 'customsearch_cwgp_script_indvclosehybrid',
+        PARENTCHILDMAPPING: 'customsearch_cwgp_script_hybridparentchi'
+    },
+    PARAMS: {
+        PARAMARRAY: 'custscript_cwgp_objcsv',
+    }
+}
+
+var LOG_NAME;
+var CSV_FOLDER = 1823567;
+define(['N/runtime','N/search','N/record','N/task','N/error','N/format','N/runtime','N/render','N/file'], function(runtime,search,record,task,error,format,runtime,render,file) {
+
+    function getInputData() {
+        LOG_NAME = 'getInputData';
+        try{
+            var paramArray = runtime.getCurrentScript().getParameter(MR_OBJ.PARAMS.PARAMARRAY);
+            var paramArrayParsed = JSON.parse(paramArray);
+            log.debug('paramArrayParsed',paramArrayParsed);
+            log.debug('paramArrayParsed Length',paramArrayParsed.length);
+        
+            return paramArrayParsed;
+        }
+        catch(e){
+            log.error(LOG_NAME,e);
+        }
+
+    }
+
+    function map(context) {
+        LOG_NAME = 'map';
+        try{
+            log.debug('value',JSON.parse(context.value));
+            var arrValues = JSON.parse(context.value);
+            var objSearch = search.load({id: MR_OBJ.SEARCH.HYBRID});
+            objSearch.filters.push(search.createFilter({name: 'internalid', operator: search.Operator.ANYOF, values: arrValues.arrCusRec})); ;
+            var searchResultCount = objSearch.runPaged().count;
+            log.debug('search results count', searchResultCount);
+            var csvContents = "";
+            csvContents += 'First Sent At' + ',' + 'Close Type' + ',' + 'Loan Reference ID' + ',' + 'Tenant' +','+ 'Parent Name' + '\n';
+            objSearch.run().each(function(result){
+                csvContents+=removeSymbol(",",result.getValue({name:"custrecord_cwgp_firstsentat"}))+','+removeSymbol(",",result.getValue({name:"custrecord_cwgp_closingtype"}))+','+removeSymbol(",",result.getValue({name:"custrecord_cwgp_loanreferenceid"}))+','+removeSymbol(",",result.getValue({name:"custrecord_cwgp_tenant"}))+','+removeSymbol(",",result.getValue({name:"custrecord_cwgp_parenttenanttext"})) +'\n';
+                return true;    
+            });
+            if(arrValues.hasOwnProperty('intInvId')){ 
+                if(!isEmpty(arrValues.intInvId)){
+                    var objTranId = search.lookupFields({
+                        type: search.Type.INVOICE,
+                        id: arrValues.intInvId,
+                        columns: ['tranid']
+                    });
+
+                    log.debug(objTranId);
+                    log.debug(objTranId.tranid);
+
+                    var csvFile = file.create({
+                        name:'Invoice Detailed Report_'+objTranId.tranid+'.csv',
+                        fileType: file.Type.CSV,
+                        contents: csvContents
+                    });	
+
+                    csvFile.folder = CSV_FOLDER;
+                    var csvId = csvFile.save();
+                        record.attach({
+                            record: {
+                                type: 'file',
+                                id: csvId
+                            },
+                            to: {
+                                type: 'invoice',
+                                id: arrValues.intInvId
+                            }
+                        });
+                }
+            }
+        }
+        catch(e){
+            log.error(LOG_NAME,e);
+        }
+    }
+
+    
+
+    function reduce(context) {
+        LOG_NAME = 'reduce';
+     
+    }
+
+    function summarize(summary) {
+   
+    }
+
+    
+
+
+    function isEmpty(value){
+        
+        if (value == null || value == 'null' || value == undefined || value == 'undefined' || value == '' || value == "" || value.length <= 0) 
+        { 
+            return true; 
+        }
+        return false;
+    }
+
+    function removeSymbol(symbol, str){
+        var newString = "";
+        for(var i = 0; i < str.length; i++) {
+            var char = str.charAt(i);
+            if(char != symbol){
+                newString = newString + char;
+            }
+        }
+        return newString;
+    }
+
+
+
+    return {
+        getInputData: getInputData,
+        map: map,
+        reduce: reduce,
+        summarize: summarize
+    }
+});
---
+++ BLEND_MR_CloseHybridSalesOrderToInvoice.js
@@ -0,0 +1,521 @@
+/**
+ *@NApiVersion 2.1
+ *@NScriptType MapReduceScript
+ */
+
+ 
+  /**
+ * 
+ * Date : 14 May 2021   
+ * Author : Paolo Escalona
+ * 
+ *  Date Modified       Modified By         Notes
+ *  14 May 2021         Paolo Escalona      Initial Version
+ */
+
+
+var MR_OBJ = {
+    PARAMS: {
+        PARAMARRAY: 'custscript_cwgp_objsublist',
+        DATEFROM: 'custscript_cwgp_datefrom',
+        DATETO: 'custscript_cwgp_dateto',
+        CURRENTUSER: 'custscript_cwgp_currentusersotoinv'
+    },
+    SEARCH: {
+        HYBRID: 'customsearch_cwgp_script_indvclosehybrid',
+        PARENTCHILDMAPPING: 'customsearch_cwgp_script_hybridparentchi'
+    },
+    FIELDS: {
+        TFC: 'custbody_cwgp_trmnation_fr_convenience',
+    },
+    VALUES: {
+        TYPE_HYBRID: '2',
+        TYPE_RON: '1',
+        TYPE_TRADITIONAL:'3'
+    }
+}
+
+var LOG_NAME;
+var PDF_FOLDER = 1823568;
+define(['N/runtime','N/search','N/record','N/task','N/error','N/format','N/runtime','N/render','N/file'], function(runtime,search,record,task,error,format,runtime,render,file) {
+
+    function getInputData() {
+        LOG_NAME = 'getInputData';
+        try{
+            var paramArray = runtime.getCurrentScript().getParameter(MR_OBJ.PARAMS.PARAMARRAY);
+            var paramArrayParsed = JSON.parse(paramArray);
+            log.debug('paramArrayParsed',paramArrayParsed);
+            log.debug('paramArrayParsed Length',paramArrayParsed.length);
+            return JSON.parse(paramArray);
+        }
+        catch(e){
+            log.error(LOG_NAME,e);
+        }
+    }
+
+    function map(context) {
+        LOG_NAME = 'map';
+        try{
+            var dateFrom = runtime.getCurrentScript().getParameter(MR_OBJ.PARAMS.DATEFROM);
+            var dateTo = runtime.getCurrentScript().getParameter(MR_OBJ.PARAMS.DATETO);
+
+            dateFrom = new Date(dateFrom);
+            var month = dateFrom.getUTCMonth() + 1; //months from 1-12
+            var day = dateFrom.getUTCDate();
+            var year = dateFrom.getUTCFullYear();
+            dateFrom = month+'/'+day+'/'+year;
+
+            dateTo =  new Date(dateTo);
+            var month = dateTo.getUTCMonth() + 1; //months from 1-12
+            var day = dateTo.getUTCDate();
+            var year = dateTo.getUTCFullYear();
+            dateTo = month+'/'+day+'/'+year;
+
+            var objValue = JSON.parse(context.value);
+            log.debug('searchinput', 'objValue.customerparent: ' + objValue.customerparent + '| dateFrom: ' + dateFrom + '| dateTo: ' + dateTo + '| objValue.closetype ' + objValue.closetype + '| objValue.salesorder ' + objValue.salesorder) + '| objValue.salesorderid ' + objValue.salesorderid;
+            
+
+            objValue.dateFrom = dateFrom;
+            objValue.dateTo = dateTo;
+
+            context.write({
+                key: objValue.salesorderid,
+                value: objValue
+            });
+        }
+        catch(e){
+            log.error(LOG_NAME,e);
+        }
+    }
+
+    
+
+    function reduce(context) {
+        LOG_NAME = 'reduce';
+        try{
+            log.debug(LOG_NAME, 'context.key' + context.key);
+            var values = context.values;
+            var arrValues = [];
+
+            values.forEach(function (result) {
+                log.debug(LOG_NAME,result);
+                arrValues.push(JSON.parse(result));
+            });
+
+            log.debug(LOG_NAME,arrValues);
+            log.debug('arrValues.length',arrValues.length);
+
+            var invId;
+            var isProcessed = '1';
+            var stErrorMessage;
+            //var intTFC;
+            var arrLineUniqueKey = [];
+            try{
+                var objSO = record.load({
+                    type: record.Type.SALES_ORDER,
+                    id: context.key,
+                    isDynamic: true,
+                });
+
+                /*intTFC = objSO.getValue({
+                    fieldId: MR_OBJ.FIELDS.TFC
+                });
+
+                log.debug('intTFC',intTFC);*/
+
+
+                var soItemLineCount = objSO.getLineCount({
+                    sublistId: 'item'
+                });
+
+                var intUsageCount;
+                var arrInsertItems = []
+
+                ////Copy matching RON or HYBRID Items on SO
+                for(var x = 0; x < arrValues.length; x++){
+                    intUsageCount = arrValues[x].usagecount;
+                    for(var z = 0; z < soItemLineCount; z++){
+                        objSO.selectLine({
+                            sublistId: 'item',
+                            line: z
+                        });
+
+                        var intItemCloseUsageType = objSO.getCurrentSublistValue({
+                            sublistId: 'item',
+                            fieldId: 'custcol_cwgp_closeusagetype'
+                        });
+
+                        var intItem = objSO.getCurrentSublistValue({
+                            sublistId: 'item',
+                            fieldId: 'item'
+                        });
+
+                        log.debug('intItem',intItem);
+
+                        if(intItemCloseUsageType == MR_OBJ.VALUES.TYPE_HYBRID || intItemCloseUsageType == MR_OBJ.VALUES.TYPE_RON){
+                                var intRate = objSO.getCurrentSublistValue({
+                                    sublistId: 'item',
+                                    fieldId: 'rate'
+                                });
+
+                                var objInsertItems = {
+                                    'intItem': intItem,
+                                    'intRate': intRate,
+                                    'intUsageCount' :intUsageCount,
+                                    'dateFrom': arrValues[x].dateFrom,
+                                    'dateTo': arrValues[x].dateTo,
+                                }
+                                arrInsertItems.push(objInsertItems);
+                                break;
+                        }
+                    }
+                }
+
+                if(arrInsertItems.length != arrValues.length){
+                    stErrorMessage = 'No matching item for ' + arrValues[0].salesorder;
+                    log.error('NO_MATCHING_ITEM', 'No matching item for ' + arrValues[0].salesorder);
+                }
+
+                log.debug('arrInsertItems', arrInsertItems);
+
+                ///Add new RON or HYBRID item on SO (with new usage/quantity)
+                for(var x = 0; x < arrInsertItems.length;x++){
+                    objSO.selectNewLine({
+                        sublistId: 'item'
+                    });
+                    objSO.setCurrentSublistValue({
+                        sublistId: 'item',
+                        fieldId: 'item',
+                        value: arrInsertItems[x].intItem
+                    });
+                    
+                    objSO.setCurrentSublistValue({
+                        sublistId: 'item',
+                        fieldId: 'quantity',
+                        value: parseInt(arrInsertItems[x].intUsageCount)
+                    });
+
+                    objSO.setCurrentSublistValue({
+                        sublistId: 'item',
+                        fieldId: 'rate',
+                        value: parseFloat(arrInsertItems[x].intRate)
+                    });
+
+    
+                    objSO.setCurrentSublistValue({
+                        sublistId: 'item',
+                        fieldId: 'custcol_bl_rev_rec_start',
+                        value: format.parse({value:arrInsertItems[x].dateFrom, type: format.Type.DATE, timezone : format.Timezone.AMERICA_NEW_YORK })
+                        
+                    });
+
+
+                    objSO.setCurrentSublistValue({
+                        sublistId: 'item',
+                        fieldId: 'custcol_bl_rev_rec_end',
+                        value: format.parse({value:arrInsertItems[x].dateTo, type: format.Type.DATE, timezone : format.Timezone.AMERICA_NEW_YORK })
+                    });
+
+                    
+    
+                    objSO.setCurrentSublistValue({
+                        sublistId: 'item',
+                        fieldId: 'custcol_atlas_contract_start_date',
+                        value: format.parse({value:arrInsertItems[x].dateFrom, type: format.Type.DATE, timezone : format.Timezone.AMERICA_NEW_YORK })
+                        
+                    });
+
+
+                    objSO.setCurrentSublistValue({
+                        sublistId: 'item',
+                        fieldId: 'custcol_atlas_contract_end_date',
+                        value: format.parse({value:arrInsertItems[x].dateTo, type: format.Type.DATE, timezone : format.Timezone.AMERICA_NEW_YORK })
+                    });
+
+                    
+                    ///Billing Term to Monthly Arrears
+                    objSO.setCurrentSublistValue({
+                        sublistId: 'item',
+                        fieldId: 'custcol3',
+                        value: '7'
+                    });
+                    
+
+                    objSO.commitLine({
+                        sublistId: 'item'
+                    });
+                }
+
+                var soId = objSO.save();
+
+                
+                ////Load SO and get lineuniquekeys
+                if(!isEmpty(soId)){
+                    var objSO2 = record.load({
+                        type: record.Type.SALES_ORDER,
+                        id: soId,
+                        isDynamic: true,
+                    });
+
+                    var so2itemLineCount = objSO2.getLineCount({
+                        sublistId: 'item'
+                    });
+
+                    for(var x = so2itemLineCount-1; x >= so2itemLineCount-arrInsertItems.length;x--){
+                        var intLineUniqueKey = objSO2.getSublistValue({
+                            sublistId: 'item',
+                            fieldId: 'lineuniquekey',
+                            line: x
+                        });
+                        arrLineUniqueKey.push(intLineUniqueKey);
+                    } 
+                }
+
+                ///Transfor SO to Invoice
+                var objInv = record.transform({
+                   fromType: record.Type.SALES_ORDER,
+                   fromId: context.key,
+                   toType: record.Type.INVOICE,
+                   isDynamic: true,
+                });
+
+                objInv.setValue({
+                    fieldId: 'customform',
+                    value: '158',
+                    ignoreFieldChange: false
+                });
+
+    
+                // Add lines to the invoice like this, this is the correct way when the record is in "dynamic" mode
+                var invItemLineCount = objInv.getLineCount({
+                    sublistId: 'item'
+                });
+                for(var x = invItemLineCount-1; x >= 0; x--){
+                    objInv.selectLine({
+                        sublistId: 'item',
+                        line: x
+                    });
+
+                    var intLineUniqueKey = objInv.getCurrentSublistValue({
+                        sublistId: 'item',
+                        fieldId: 'lineuniquekey'
+                    });
+
+
+                    var intItem = objInv.getCurrentSublistValue({
+                        sublistId: 'item',
+                        fieldId: 'item'
+                    });
+
+                    
+                    var stDateFrom = objInv.getCurrentSublistValue({
+                        sublistId: 'item',
+                        fieldId: 'custcol_bl_rev_rec_start'
+                    });
+
+                    var stDateTo = objInv.getCurrentSublistValue({
+                        sublistId: 'item',
+                        fieldId: 'custcol_bl_rev_rec_end'
+                    });
+
+                            
+                    stDateFrom = new Date(stDateFrom);
+                    var month = stDateFrom.getUTCMonth() + 1; //months from 1-12
+                    var day = stDateFrom.getUTCDate();
+                    var year = stDateFrom.getUTCFullYear();
+                    stDateFrom = month+'/'+day+'/'+year;
+
+                    stDateTo =  new Date(stDateTo);
+                    var month = stDateTo.getUTCMonth() + 1; //months from 1-12
+                    var day = stDateTo.getUTCDate();
+                    var year = stDateTo.getUTCFullYear();
+                    stDateTo = month+'/'+day+'/'+year;
+
+                    
+                    log.debug('intItem | stDateFrom | stDateTo',intItem + '|' + stDateFrom + '|' + stDateTo);
+
+                    if(arrLineUniqueKey.includes(intLineUniqueKey) == false){
+                        log.debug('Remove Line: ' + x);
+                        objInv.removeLine({
+                            sublistId: 'item',
+                            line: x,
+                            ignoreRecalc: false
+                        });
+                    }
+                    else{
+                        objInv.setCurrentSublistValue({
+                            sublistId: 'item',
+                            fieldId: 'custcol3',
+                            value: '7'
+                        });
+
+                        var stDateFrom = objInv.getCurrentSublistValue({
+                            sublistId: 'item',
+                            fieldId: 'custcol_bl_rev_rec_start'
+                        });
+    
+                        var stDateTo = objInv.getCurrentSublistValue({
+                            sublistId: 'item',
+                            fieldId: 'custcol_bl_rev_rec_end'
+                        });
+
+                        log.debug('stDateFrom | stDateTo', 'stDateFrom: ' + stDateFrom + '| stDateTo: '  + stDateTo);
+    
+
+                        objInv.setCurrentSublistValue({
+                            sublistId: 'item',
+                            fieldId: 'custcol_inv_startdatestored',
+                            value: format.parse({value:stDateFrom, type: format.Type.DATE})
+                        });
+    
+                        objInv.setCurrentSublistValue({
+                            sublistId: 'item',
+                            fieldId: 'custcol_inv_enddatestored',
+                            value: format.parse({value:stDateTo, type: format.Type.DATE})
+                        });
+            
+                        objInv.commitLine({
+                            sublistId: 'item'
+                        });
+                    }
+                }
+                // Submit the record       
+                invId = objInv.save();
+                
+                //Create Invoice PDF if Invoice record is created
+                if(!isEmpty(invId)){
+                    var transactionFile = render.transaction({
+                        entityId: invId,
+                        printMode: render.PrintMode.PDF,
+                        inCustLocale: true
+                        });
+                    
+                    transactionFile.folder = PDF_FOLDER;
+                    var pdfId = transactionFile.save();
+                        
+                    record.attach({
+                        record: {
+                            type: 'file',
+                            id: pdfId
+                        },
+                        to: {
+                            type: 'invoice',
+                            id: invId
+                        }
+                    });
+                }
+
+            }
+            catch(e){
+                invId = null;
+                isProcessed = '2';
+                stErrorMessage = e.message;
+
+                log.error(LOG_NAME,e);
+            }
+
+            log.debug('invId',invId);
+            for(var x = 0; x < arrValues.length; x++){
+                arrValues[x].paramProcessed = isProcessed;
+                arrValues[x].paramInvId = invId;
+                arrValues[x].paramErrorMessage = stErrorMessage;
+            }
+
+            log.debug('arrValues',arrValues);
+
+            context.write({
+                key: arrValues[0].customerparent,
+                value: arrValues
+            });
+        }
+        catch(e){
+           log.error(LOG_NAME,e);
+       }
+        
+    }
+
+    function summarize(summary) {
+        LOG_NAME = 'summarize';
+        try{
+            log.debug('summary','summary');
+            var arrValues = [];
+            summary.output.iterator().each(function (key, value)
+            {
+                log.debug('summary','key: ' + key + '| value: ' + value);
+                arrValues.push(JSON.parse(value));
+                return true;
+            });
+            log.debug('summary arr',arrValues);
+   
+            var params = new Object();
+            params['custscript_cwgp_objrec'] = arrValues;
+            params['custscript_cwgp_currentuserupdaterec'] = runtime.getCurrentScript().getParameter(MR_OBJ.PARAMS.CURRENTUSER);
+
+
+
+            var mrTask = task.create({
+                taskType: task.TaskType.MAP_REDUCE,
+                scriptId: 'customscript_cwgp_mr_clsehybridupdaterec',
+            });
+
+            mrTask.params = params;
+            mrTaskId = mrTask.submit();
+        }
+        catch(e){
+            log.error(LOG_NAME,e);
+        }
+    }
+
+    
+
+
+    function isEmpty(value){
+        
+        if (value == null || value == 'null' || value == undefined || value == 'undefined' || value == '' || value == "" || value.length <= 0) 
+        { 
+            return true; 
+        }
+        return false;
+    }
+
+    function getAllResults(s) {
+        var results = s.run();
+        var searchResults = [];
+        var searchid = 0;
+        do {
+            var resultslice = results.getRange({start:searchid,end:searchid+1000});
+            resultslice.forEach(function(slice) {
+                searchResults.push(slice);
+                searchid++;
+                }
+            );
+        } while (resultslice.length >=1000);
+        return searchResults;
+    }   
+
+    function dateToString(dt){
+        return ((dt.getMonth() > 8) ? (dt.getMonth() + 1) : ('0' + (dt.getMonth() + 1))) + '/' + ((dt.getDate() > 9) ? dt.getDate() : ('0' + dt.getDate())) + '/' + dt.getFullYear()    
+    }
+
+    
+    function formatDate(date) {
+        return (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+    }
+
+    function formatDateToString(date){
+        var today = new Date(date);
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        return today = mm + '/' + dd + '/' + yyyy;
+    }
+
+
+    return {
+        getInputData: getInputData,
+        map: map,
+        reduce: reduce,
+        summarize: summarize
+    }
+});
