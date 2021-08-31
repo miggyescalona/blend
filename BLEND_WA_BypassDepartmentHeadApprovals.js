894915e49f0fbd05aceeed2ee33184af701df38d
Miggy Escalona <miggyescalona@users.noreply.github.com>
2021-08-31 11:33:53 -0400

    Add files via upload
     *  17 June 2021        Paolo Escalona      Initial Version
     *  7 July 2021         Paolo Escalona      Modified to accommodate all approver types (except Role)

--- BLEND_WA_BypassDepartmentHeadApprovals.js
+++ BLEND_WA_BypassDepartmentHeadApprovals.js
@@ -1,19 +1,28 @@
-function bypassDepartmentHeadApprover(context){
+/**
+ * 
+ * Date : 17 June 2021
+ * Author : Paolo Escalona
+ * 
+ *  Date Modified       Modified By         Notes
+ *  17 June 2021        Paolo Escalona      Initial Version
+ *  7 July 2021         Paolo Escalona      Modified to accomodate all approver types (execept Role)
+ */
+
+ function bypassDepartmentHeadApprover(context){
     try{
 
         var arrApprovers = [];
+        var currentApprover = [];
         var recId = nlapiGetRecordId();
         var customrecord_nsts_gaw_approver_listSearch = nlapiSearchRecord("customrecord_nsts_gaw_approver_list",null,
             [
             ["custrecord_nsts_gaw_po_rec_type","anyof",recId], 
-            "AND", 
-            ["custrecord_nsts_gaw_porulename","anyof","1","2","3"]
             ], 
             [
             new nlobjSearchColumn("custrecord_nsts_gaw_rulesequence"), 
             new nlobjSearchColumn("custrecord_nsts_gaw_po_rec_type"), 
             new nlobjSearchColumn("custrecord_nsts_gaw_tran_approver"), 
-            new nlobjSearchColumn("custrecord_nsts_gaw_list_date"), 
+            new nlobjSearchColumn("custrecord_nsts_gaw_list_date").setSort(false), 
             new nlobjSearchColumn("custrecord_nsts_gaw_approverrole"), 
             new nlobjSearchColumn("custrecord_nsts_gaw_porulename"), 
             new nlobjSearchColumn("custrecord_nsts_gaw_approverlinestatus"), 
@@ -22,21 +31,40 @@ function bypassDepartmentHeadApprover(context){
             ]
         );
         if(!isEmpty(customrecord_nsts_gaw_approver_listSearch)){
-            if(customrecord_nsts_gaw_approver_listSearch.length== 2) {
+            if(customrecord_nsts_gaw_approver_listSearch.length > 1) {
                 for (var i = 0 ; i < customrecord_nsts_gaw_approver_listSearch.length; i++) {
                     obj = {
                         intApprover: customrecord_nsts_gaw_approver_listSearch[i].getValue('custrecord_nsts_gaw_tran_approver'),
                         intApproverType: customrecord_nsts_gaw_approver_listSearch[i].getValue('custrecord_nsts_gaw_porulename'),
                         intApprovalStatus: customrecord_nsts_gaw_approver_listSearch[i].getValue('custrecord_nsts_gaw_approverlinestatus'),
                     }
-                    arrApprovers.push(obj);
+                    if(i == customrecord_nsts_gaw_approver_listSearch.length-1){
+                        currentApprover.push(obj);
+                    }
+                    else if(customrecord_nsts_gaw_approver_listSearch[i].getValue('custrecord_nsts_gaw_approverlinestatus') == '3'){
+                        currentApprover = [];
+                    }
+                    else{
+                        arrApprovers.push(obj);
+                    }
                 };
 
-                nlapiLogExecution('DEBUG','arrApprovers',arrApprovers);
                 nlapiLogExecution('DEBUG','arrApprovers stringified', JSON.stringify(arrApprovers));
+                nlapiLogExecution('DEBUG','arrApprovers stringified', JSON.stringify(currentApprover));
 
-                if(arrApprovers[0].intApprover == arrApprovers[1].intApprover && arrApprovers[0].intApprovalStatus == '2' && arrApprovers[0].intApproverType == '2' && arrApprovers[1].intApproverType == '1'){
-                    return 1;
+              	if(currentApprover.length > 0){
+                  if(!isEmpty(currentApprover[0].intApprover) && isEmpty(currentApprover[0].intApprovalStatus)){
+                      for(var x = 0; x < arrApprovers.length;x++){
+                          if(arrApprovers[x].intApprover == currentApprover[0].intApprover){
+                              return 1;
+                              break;
+                          }
+                      }
+                      return 0;
+                  }
+                  else{
+                    return 0;
+                  }
                 }
                 else{
                     return 0;
